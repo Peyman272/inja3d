21b0d026da8d443f9414cedc4d7ee09708180fc4
Peyman272 <peymanpj@outlook.com>
2026-07-14 02:35:11 +0330

    Update route.ts

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -5,15 +5,13 @@ export async function GET() {
   try {
     const { data } = await wcFetch("/products/categories", {
       per_page: 100,
-
-      // 💣 مهم: همه دسته‌ها رو بیار
-      hide_empty: "false",
-
+      hide_empty: false,
       orderby: "name",
       order: "asc",
     });
 
     return NextResponse.json(data);
+
   } catch (err: any) {
     return NextResponse.json(
       { error: err.message ?? "خطای ناشناخته" },
