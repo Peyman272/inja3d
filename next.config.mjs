b5f14facf89081bdb105c44492771b2062e371a7
Peyman272 <peymanpj@outlook.com>
2026-07-16 20:20:47 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/product/:slug',
+        destination: '/products/:slug',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
