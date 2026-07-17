9649654e03b641a990b8d65a3d39d428cbd72201
Peyman272 <peymanpj@outlook.com>
2026-07-18 02:47:43 +0330

    Update route.ts

--- app/api/torob/v3/products/route.ts
+++ app/api/torob/v3/products/route.ts
@@ -1,1 +1,10 @@
+import { NextResponse } from "next/server";
 
+export async function POST() {
+
+  return NextResponse.json({
+    api_version: "torob_api_v3",
+    message: "Torob API is working"
+  });
+
+}
