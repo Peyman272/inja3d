3b8ac097cf94689658f652e8b078362661e06d49
Peyman272 <peymanpj@outlook.com>
2026-07-18 02:35:52 +0330

    Update response.ts

--- lib/torob/response.ts
+++ lib/torob/response.ts
@@ -1,1 +1,27 @@
+import { TorobProduct, TorobResponse } from "./types";
 
+
+export function createTorobResponse(
+  products: TorobProduct[],
+  page: number,
+  total: number
+): TorobResponse {
+
+  return {
+
+    api_version: "torob_api_v3",
+
+    current_page: page,
+
+    total: total,
+
+    max_pages: Math.max(
+      1,
+      Math.ceil(total / 100)
+    ),
+
+    products
+
+  };
+
+}
