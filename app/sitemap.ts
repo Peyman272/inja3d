54b8d5b889421d17940df062d4f52722a410966d
Peyman272 <peymanpj@outlook.com>
2026-07-21 20:33:58 +0330

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,26 @@
+import { MetadataRoute } from "next";
 
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+
+const products = await fetch(
+"https://wp.inja3d.ir/wp-json/wc/v3/products"
+).then(res => res.json());
+
+
+const productUrls = products.map((product:any)=>({
+ url:`https://www.inja3d.ir/products/${product.slug}`,
+ lastModified:new Date(),
+}));
+
+
+return [
+{
+ url:"https://www.inja3d.ir",
+ lastModified:new Date(),
+},
+
+...productUrls
+
+];
+
+}
